b1cfef7c8832bf48dad2547d4b49c88e69f0bbe4
afcj8 <contatoantoniojunior8@gmail.com>
2024-10-03 12:34:52 -0300

    exemplo do método startsWith

---
+++ 19 - Principais Métodos da Classe String/MetodoStartsWith.java
@@ -0,0 +1,10 @@
+public class MetodoStartsWith {
+    public static void main(String[] args) {
+        String nome = "Antônio Junior";
+        if (nome.startsWith("Antônio")) {
+            System.out.println("A String começa com Antônio");
+        } else {
+            System.out.println("A String não começa com Antônio");
+        }
+    }
+}
